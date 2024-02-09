e5a5204e78cfed468c6b0ed0ff9ea4fc9cb2abf7
Administrator <losdientesdeava@gmail.com>
2024-02-09 14:04:45 -0800

    Senior Meet

---
+++ src/main/java/com/seniormeet/SeniormeetApplication.java
@@ -0,0 +1,13 @@
+package com.seniormeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniormeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniormeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seniormeet/SeniormeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniormeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniormeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
