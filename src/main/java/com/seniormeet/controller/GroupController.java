c58e1dd4fa44c967fae01a0980b6e504df2a2cff
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-02-17 20:00:52 +0100

    creado usergroup, groupservice,

---
+++ src/main/java/com/seniormeet/controller/GroupController.java
@@ -0,0 +1,35 @@
+package com.seniormeet.controller;
+
+import com.seniormeet.model.Group;
+import com.seniormeet.repository.GroupRepository;
+import com.seniormeet.service.GroupService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/groups")
+public class GroupController {
+
+    private GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Group>> findAll() {
+        List<Group> groups = groupService.findGroup();
+        return ResponseEntity.ok(groups);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Group> findById(@PathVariable Long id){
+        Group group = groupService.findById(id);
+        return ResponseEntity.ok(group);
+    }
+}
--- src/main/java/com/seniormeet/model/Group.java
+++ src/main/java/com/seniormeet/model/Group.java
@@ -2,6 +2,9 @@ package com.seniormeet.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "sm_group")
 public class Group {
@@ -16,5 +19,8 @@ public class Group {
     private String rules;
     private String photoUrl;
 
-
+    private String user;
+    //puse esto para relacionar con USUARIO
+    @ManyToMany
+    private List<User> users = new ArrayList<>();
 }
