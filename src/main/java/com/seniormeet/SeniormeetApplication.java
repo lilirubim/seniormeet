8d80a5263705ecb61845b90173d21ba5fbec77a7
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-02-24 18:39:19 +0100

    falta testear postmapping, putmapping, deletemapping

--- src/main/java/com/seniormeet/SeniormeetApplication.java
+++ src/main/java/com/seniormeet/SeniormeetApplication.java
@@ -1,13 +1,29 @@
 package com.seniormeet;
 
+import com.seniormeet.controller.GroupController;
+import com.seniormeet.model.Group;
+import com.seniormeet.repository.GroupRepository;
+import com.seniormeet.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.DeferredImportSelector;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SeniormeetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeniormeetApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SeniormeetApplication.class, args);
+
+		GroupRepository groupRepo = context.getBean(GroupRepository.class);
+
+		Group group1 = new Group(null, "Naturaleza", "Amantes de la Naturaleza", "Ganas de aventura", null, "1", List.of());
+		Group group2 = new Group(null, "Cultura", "Cine, teatro, espectáculos, museos", "sin alcohol", null, "2", List.of());
+
+		groupRepo.saveAll(List.of(group1,group2));
 	}
 
 }
