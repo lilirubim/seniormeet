0c99ff1238d96ed23940319676bbea596e689d7b
noemihd76 <noemihd76@gmail.com>
2024-02-26 19:37:39 +0100

    Hobby controller CRUD

--- src/main/java/com/seniormeet/SeniormeetApplication.java
+++ src/main/java/com/seniormeet/SeniormeetApplication.java
@@ -1,8 +1,8 @@
 package com.seniormeet;
 
+import com.seniormeet.model.Hobby;
 import com.seniormeet.model.Interaction;
-import com.seniormeet.model.Post;
-import com.seniormeet.model.User;
+import com.seniormeet.repository.HobbyRepository;
 import com.seniormeet.repository.InteractionRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,6 +25,14 @@ public class SeniormeetApplication {
 		Interaction interaction3 = new Interaction(null, true, LocalDateTime.now(), true, LocalDateTime.now(), true, LocalDateTime.now(), "felicidades", LocalDateTime.now(), null, null, List.of());
 
 		interactionRepo.saveAll(List.of(interaction1, interaction2, interaction3));
+
+
+		HobbyRepository hobbyRepo = context.getBean(HobbyRepository.class);
+
+		Hobby hobby1 = new Hobby(null, "Balar Merengue", "divertida clase de Merengue ");
+		Hobby hobby2 = new Hobby(null, "Paseo por el Retiro","lunes por la mañana a las 10 ");
+		Hobby hobby3 = new Hobby(null, "Salida al Teatro", "dia 29 de febrero a las 19 h ");
+		hobbyRepo.saveAll(List.of(hobby1, hobby2, hobby3));
 	}
 
 }
--- src/main/java/com/seniormeet/service/HobbyServiceImpl.java
+++ src/main/java/com/seniormeet/service/HobbyServiceImpl.java
@@ -5,6 +5,7 @@ import com.seniormeet.repository.HobbyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HobbyServiceImpl implements HobbyService {
@@ -27,5 +28,32 @@ public class HobbyServiceImpl implements HobbyService {
         return hobbyRepository.findById(id).orElse(null);
     }
 
+    @Override
+    public Hobby updateHobby(Long id, Hobby hobby) {
+        Optional<Hobby> hobbyOptional = hobbyRepository.findById(id);
+        if (hobbyOptional.isPresent()) {
+            Hobby existingHobby = hobbyOptional.get();
+            existingHobby.setName(hobby.getName());
+            existingHobby.setDescription(hobby.getDescription());
+            return hobbyRepository.save(existingHobby);
+        } else {
+        return null;}
+    }
+
+    @Override
+    public Hobby save(Hobby hobby) {
+        return hobbyRepository.save(hobby);
+    }
+
+    @Override
+    public boolean deleteHobby(Long id) {
+            Optional<Hobby> hobbyOptional = hobbyRepository.findById(id);
+            if (hobbyOptional.isPresent()) {
+                hobbyRepository.deleteById(id);
+                return true;
+            } else {
+                return false;
+            }
+        }
 
 }
