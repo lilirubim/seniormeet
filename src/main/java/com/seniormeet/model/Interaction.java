d0f661d4e1c035accbbcfb8d444c34201d76150c
lilirubim <148784760+lilirubim@users.noreply.github.com>
2024-03-12 21:51:04 +0100

    Asociaciones @ManytoOne User y Post;

--- src/main/java/com/seniormeet/model/Interaction.java
+++ src/main/java/com/seniormeet/model/Interaction.java
@@ -1,12 +1,11 @@
 package com.seniormeet.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 
@@ -39,16 +38,5 @@ public class Interaction {
     @JoinColumn(name = "post_id")
     private Post post;
 
-//    @ManyToMany(mappedBy = "interactions")
-//    @JsonIgnoreProperties("interactions")
-//    @ToString.Exclude
-//    /*@JoinTable(
-//            name = "sm_post_interactions",
-//            joinColumns = @JoinColumn(name = "interaction_id"),
-//            inverseJoinColumns = @JoinColumn(name = "post_id")
-//    )
-//
-//     */
-//    private List<Post> posts = new ArrayList<>();
 
 }
