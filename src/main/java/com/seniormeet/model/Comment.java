d72dbcb762f120a3c79f97e5fd6df80f9ca83516
fran-eliot <ramirez.martin.francisco@gmail.com>
2024-04-04 11:02:45 +0200

    Agregando los métodos equals y hashCode en los modelos Interaction y Comment, a fin de pode comparar sus objetos

--- src/main/java/com/seniormeet/model/Comment.java
+++ src/main/java/com/seniormeet/model/Comment.java
@@ -2,6 +2,9 @@ package com.seniormeet.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -22,5 +25,19 @@ public class Comment {
     @JoinColumn(name="user_id")
     User user;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Comment comment = (Comment) o;
+        return getId() != null && Objects.equals(getId(), comment.getId());
+    }
 
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
