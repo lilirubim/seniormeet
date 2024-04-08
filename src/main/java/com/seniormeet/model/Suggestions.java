432f0a51e84e43135314df95130cd3d5b1243219
noemihd76 <noemihd76@gmail.com>
2024-04-08 21:36:18 +0200

    SuggestionsController, model, repository y service

--- src/main/java/com/seniormeet/model/Suggestions.java
+++ src/main/java/com/seniormeet/model/Suggestions.java
@@ -1,2 +1,25 @@
-package com.seniormeet.controller;public class Suggestions {
+package com.seniormeet.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sm_suggestions")
+public class Suggestions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+
+
 }
