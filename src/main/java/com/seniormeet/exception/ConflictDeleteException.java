a65b985830c8ee00fda0dbc73c50449594de750a
AngelSigueros <pickassblog@gmail.com>
2024-04-24 21:40:50 +0200

    Nuevas excepciones

---
+++ src/main/java/com/seniormeet/exception/ConflictDeleteException.java
@@ -0,0 +1,9 @@
+package com.seniormeet.exception;
+
+public class ConflictDeleteException extends RuntimeException {
+    public ConflictDeleteException(String message) {
+
+        super(message);
+    }
+}
+
