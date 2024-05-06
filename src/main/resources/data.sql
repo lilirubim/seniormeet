7e290ea7c42295126db79e67e6ee8ede8e132b29
AngelSigueros <pickassblog@gmail.com>
2024-05-06 19:52:47 +0200

    post id group

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,15 +11,15 @@ INSERT INTO sm_user (email, first_name, last_name, password, phone, photo_url, r
 INSERT INTO sm_user (email, first_name, last_name, password, phone, photo_url, role, available) VALUES ('judith@gmail.com', 'Judith', 'Blandin', '$2a$10$Da/0KQVt4FusDlhQk2n9Lu2olKG1aYB3.Bezn3DaH0kKngMxznP1W', '666777888', 'judith.jpg', 'USER', 1);
 INSERT INTO sm_user (email, first_name, last_name, password, phone, photo_url, role, available) VALUES ('@gmail.com', 'Javier', 'Baeza-Rojano', '$2a$10$Da/0KQVt4FusDlhQk2n9Lu2olKG1aYB3.Bezn3DaH0kKngMxznP1W', '666777888', 'javier.jpg', 'USER', 1);
 -- sm_group
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Entusiastas de los paseos, respeto por la naturaleza', 'caminata.jpg', 'Sábado 08:00hrs, Parque del Buen Retiro', 'Caminatas');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Enamorados de la fotografía, comparte tus mejores fotos y trucos', 'foto.jpg', ' Viernes 18:00hrs, Principe de Vergara 110', 'Club de Fotografía');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Cocina saludable y nutrition, ven y comparte tus recetas', 'cocina.jpg', 'Jueves 17:00hrs, Centro cultural "Edad Dorada"', 'Club de Cocina');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Comedores libro, debate sobre tus libros y autores favoritos', 'lectura.jpg', 'Lunes y miércoles 17:00hrs, Biblioteca "José deL Hierro"', 'Club de lectura');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Grupo de baile, aprende todos los ritmos', 'baile.jpg', 'Viernes 19.00hrs, Escuela de Danza "Elena Rojo"', 'Bailadores');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Comunidad de arte y pintura, comparte tus técnicas y obras de arte', 'arte.jpg', 'Martes 16:00hrs, Centro cultural "Buena Esperanza"', 'Arte y Pintura');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Viajeros exploradores, viajes y nuevos destinos', 'viajeros.jpg', 'Lunes 19:00hrs, Biblioteca "José deL Hierro"', 'Pasión por Viajar');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Amantes de la música, Hablamos de música, solistas y  bandas', 'musica.png', 'Miércoles 18:00hrs, Centro cultural "Edad Dorada"', 'Club de música');
-INSERT INTO sm_group (description, photo_url, rules, title) VALUES ('Huertos urbanos, promovemos los huertos sostenible', 'huerto.jpg', 'Viernes 10:00hrs, parque "Pradolongo', 'Huerto');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (1, 'Entusiastas de los paseos, respeto por la naturaleza', 'caminata.jpg', 'Sábado 08:00hrs, Parque del Buen Retiro', 'Caminatas');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (2, 'Enamorados de la fotografía, comparte tus mejores fotos y trucos', 'foto.jpg', ' Viernes 18:00hrs, Principe de Vergara 110', 'Club de Fotografía');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (3, 'Cocina saludable y nutrition, ven y comparte tus recetas', 'cocina.jpg', 'Jueves 17:00hrs, Centro cultural "Edad Dorada"', 'Club de Cocina');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (4, 'Comedores libro, debate sobre tus libros y autores favoritos', 'lectura.jpg', 'Lunes y miércoles 17:00hrs, Biblioteca "José deL Hierro"', 'Club de lectura');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (5, 'Grupo de baile, aprende todos los ritmos', 'baile.jpg', 'Viernes 19.00hrs, Escuela de Danza "Elena Rojo"', 'Bailadores');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (6, 'Comunidad de arte y pintura, comparte tus técnicas y obras de arte', 'arte.jpg', 'Martes 16:00hrs, Centro cultural "Buena Esperanza"', 'Arte y Pintura');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (7, 'Viajeros exploradores, viajes y nuevos destinos', 'viajeros.jpg', 'Lunes 19:00hrs, Biblioteca "José deL Hierro"', 'Pasión por Viajar');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (8, 'Amantes de la música, Hablamos de música, solistas y  bandas', 'musica.png', 'Miércoles 18:00hrs, Centro cultural "Edad Dorada"', 'Club de música');
+INSERT INTO sm_group (id, description, photo_url, rules, title) VALUES (9, 'Huertos urbanos, promovemos los huertos sostenible', 'huerto.jpg', 'Viernes 10:00hrs, parque "Pradolongo', 'Huerto');
 -- sm_hobby
 INSERT INTO `sm_hobby` (`description`, `name`, `photo_url`) VALUES
 ('Escuchar música, cantar o aprender a tocar un instrumento. La música es el lenguaje del alma','Música','musica.jpg'),
@@ -33,17 +33,17 @@ INSERT INTO `sm_hobby` (`description`, `name`, `photo_url`) VALUES
 ('Tomar fotos de la familia, paisajes o momentos especiales.','Fotografía','fotografia.jpg'),
 ('Escribir historias, poemas o llevar un diario personal. La escritura es una excelente manera de expresar pensamientos y sentimientos, y puede ser una actividad muy gratificante.','Escribir','escribiendo.jpg');
 -- sm_post
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (1, 'Gran contenido', 'grancontenido.png', null, '2024-02-01');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (1, 'Articulo interesante', 'articulo.jpg', null, '2024-01-12');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (3, 'Nuevo grupo de Cocina', 'cocinando.jpg', null, '2024-03-15');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (10, 'Nuevo Producto', 'nuevoproducto.jpg', null, '2024-03-28');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (7, 'Aficiones Excitantes!', 'baile.jpg', null, '2024-02-11');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (6, 'Divertido meme', 'meme.jpg', null, '2024-01-01');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (4, 'Cita importante', 'evento.jpg', null, '2024-04-26');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (2, 'Te invito a la reflexión', 'cita.jpg', null, '2024-04-30');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (8,'Feliz Viernes', 'happyFridays.jpg', null, '2024-04-22');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (5,'Recordatorio de  evento', 'evento.jpg', null, '2024-04-30');
-INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`) VALUES (11,'Feliz Halloween!', 'halloween.jpg' , null, '2023-10-31');
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (1, 'Gran contenido', 'grancontenido.png', null, '2024-02-01', 1);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (1, 'Articulo interesante', 'articulo.jpg', null, '2024-01-12', 2);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (3, 'Nuevo grupo de Cocina', 'cocinando.jpg', null, '2024-03-15', 3);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (10, 'Nuevo Producto', 'nuevoproducto.jpg', null, '2024-03-28', 4);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (7, 'Aficiones Excitantes!', 'baile.jpg', null, '2024-02-11', 5);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (6, 'Divertido meme', 'meme.jpg', null, '2024-01-01', 6);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (4, 'Cita importante', 'evento.jpg', null, '2024-04-26', 7);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (2, 'Te invito a la reflexión', 'cita.jpg', null, '2024-04-30', 8);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (8,'Feliz Viernes', 'happyFridays.jpg', null, '2024-04-22', 9);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (5,'Recordatorio de  evento', 'evento.jpg', null, '2024-04-30', 1);
+INSERT INTO sm_post (`user_id`, `content`, `photo_url`, `video_url`, `date`, `group_id`) VALUES (11,'Feliz Halloween!', 'halloween.jpg' , null, '2023-10-31', 2);
 -- sm_interaction
 INSERT INTO sm_interaction (date, id, post_id, user_id, type) VALUES ('2022-01-12', 1, 1, 1, 'LIKE');
 INSERT INTO sm_interaction (date, id, post_id, user_id, type) VALUES ('2022-01-12', 2, 2, 2, 'SAVE');
